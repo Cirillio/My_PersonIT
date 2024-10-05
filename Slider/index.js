cd891d59ba9894e4ad497b9ef52cfc2083ba019c
Cirillio <ineed2healing@gmail.com>
2024-10-05 15:09:13 +0700

    Калькулятор готов.

--- Slider/index.js
+++ Slider/index.js
@@ -53,3 +53,141 @@ dotsWrapper.addEventListener("click", (e) => {
     }
   }
 });
+
+let orderMen = document.querySelector("#order-men"),
+  orderPhotos = document.querySelector("#order-photos"),
+  orderGenre = document.querySelector("#order-genre"),
+  orderCalc = document.querySelector("#order-calc");
+
+let persons = 0,
+  photos = 0,
+  total = 0;
+
+let genres = [
+  {
+    genre: "Черно-белая",
+    price: 1000,
+  },
+  {
+    genre: "Цветная",
+    price: 1500,
+  },
+  {
+    genre: "Минимализм",
+    price: 2000,
+  },
+];
+
+let lastPersons = 0,
+  lastPhotos = 0,
+  lastTotal = 0;
+
+orderCalc.value = "0";
+
+orderMen.addEventListener("keydown", (e) => {
+  if (
+    e.key === "Backspace" ||
+    e.key === "Delete" ||
+    e.key === "ArrowLeft" ||
+    e.key === "ArrowRight" ||
+    e.key === "Home" ||
+    e.key === "End" ||
+    (e.ctrlKey &&
+      (e.key === "a" ||
+        e.key === "c" ||
+        e.key === "x" ||
+        e.key === "v" ||
+        e.key === "z" ||
+        e.key === "y"))
+  ) {
+    return;
+  }
+  if (!/\d/.test(e.key)) {
+    e.preventDefault();
+  }
+});
+
+orderMen.addEventListener("paste", (e) => {
+  const clipboardData = e.clipboardData || window.clipboardData;
+
+  const pastedText = clipboardData.getData("Text");
+
+  if (!/^\d+$/.test(pastedText)) {
+    e.preventDefault();
+  }
+});
+
+orderPhotos.addEventListener("keydown", (e) => {
+  if (
+    e.key === "Backspace" ||
+    e.key === "Delete" ||
+    e.key === "ArrowLeft" ||
+    e.key === "ArrowRight" ||
+    e.key === "Home" ||
+    e.key === "End" ||
+    (e.ctrlKey &&
+      (e.key === "a" ||
+        e.key === "c" ||
+        e.key === "x" ||
+        e.key === "v" ||
+        e.key === "z" ||
+        e.key === "y"))
+  ) {
+    return;
+  }
+  if (!/\d/.test(e.key)) {
+    e.preventDefault();
+  }
+});
+
+orderPhotos.addEventListener("paste", (e) => {
+  const clipboardData = e.clipboardData || window.clipboardData;
+
+  const pastedText = clipboardData.getData("Text");
+
+  if (!/^\d+$/.test(pastedText)) {
+    e.preventDefault();
+  }
+});
+
+orderMen.addEventListener("input", function () {
+  persons = this.value;
+  if (lastPersons === persons) {
+    return;
+  }
+  lastPersons = persons;
+
+  total = persons * photos * genres[orderGenre.value - 1].price;
+
+  if (orderPhotos.value === "" || orderMen.value === "") {
+    orderCalc.value = 0;
+  } else {
+    orderCalc.value = total;
+  }
+});
+
+orderPhotos.addEventListener("input", function () {
+  photos = this.value;
+  if (lastPhotos === photos) {
+    return;
+  }
+  lastPhotos = photos;
+
+  total = persons * photos * genres[orderGenre.value - 1].price;
+
+  if (orderPhotos.value === "" || orderMen.value === "") {
+    orderCalc.value = 0;
+  } else {
+    orderCalc.value = total;
+  }
+});
+
+orderGenre.addEventListener("input", function () {
+  total = persons * photos * genres[this.value - 1].price;
+
+  if (orderMen.value === "" || orderPhotos.value === "") {
+    orderCalc.value = 0;
+  } else {
+    orderCalc.value = total;
+  }
+});
