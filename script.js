d397b3addc993fd6df0774af1a4b6298eb1b34af
Cirillio <ineed2healing@gmail.com>
2024-11-30 21:05:51 +0700

    Поработал над адаптивностью + сделал слайдер.

--- script.js
+++ script.js
@@ -32,4 +32,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const menu = document.querySelector(".menu");
     menu.classList.toggle("menu--active");
   });
+
+  // Слайдер
+  document.querySelectorAll(".container__block--slider").forEach((slider) => {
+    const slides = slider.querySelector(".container__block--slides");
+    const slideCount = slider.querySelectorAll(
+      ".container__block--slide"
+    ).length;
+    let currentIndex = 0;
+
+    slider.querySelector(".slider-next").addEventListener("click", () => {
+      currentIndex = (currentIndex + 1) % slideCount;
+      slides.classList.add("slide-transition"); // Класс для плавности прокрутки
+      updateSlider();
+    });
+
+    slider.querySelector(".slider-back").addEventListener("click", () => {
+      currentIndex = (currentIndex - 1 + slideCount) % slideCount;
+      slides.classList.add("slide-transition");
+      updateSlider();
+    });
+
+    function updateSlider() {
+      const sliderWidth = slider.clientWidth; // Текущая ширина слайдера
+      const offset = -currentIndex * sliderWidth;
+      slides.style.transform = `translateX(${offset}px)`;
+    }
+    window.addEventListener("resize", () => {
+      slides.classList.remove("slide-transition"); // убираю класс чтобы изображение подстраивалось незаметно
+      updateSlider(); // Пересчитываю положение текущего слайда
+    });
+  });
 });
